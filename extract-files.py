063ddc0ea5cd1bfd001cbd851e18e60b24c23a1f
pabloescobar-reborn <yogivangala07@gmail.com>
2025-05-07 21:45:29 +0530

    marble: Add Soong namespace imports for required hardware components

--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,10 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/xiaomi/sm8450-common',
+    'hardware/google/interfaces',
+    'hardware/google/pixel',
+    'hardware/lineage/interfaces/power-libperfmgr',
+    'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8450',
     'hardware/xiaomi',
     'vendor/qcom/opensource/commonsys-intf/display',
